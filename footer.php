3e697467222a9327e1eadd5ad19e183dabe2fec2
Lydia King <muckaluck525@yahoo.com>
2014-11-03 22:09:30 -0800

    zdrbzr

--- footer.php
+++
@@ -1,8 +0,0 @@
-</div><!--content closing div-->
-<footer>
-        <?php get_sidebar("footer"); ?>
-	<p><small>Copyright &#169; 2014, Neighborhood Emergency Communications Network | All Rights Reserved</small></p>
-</footer>
-<?php wp_footer(); ?>
-</body>
-</html>
